6ee8605bf8e340429dd7617e77b34c6eb95fdade
David Lubin <lubin.davidg@gmail.com>
2023-05-08 23:14:29 -0400

    Basic layout and getting started on styles

---
+++ components/header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+    return (<header className="fixed top-0 w-full px-2 h-14 flex items-center justify-between bg-indigo-950 text-slate-400 border-b border-slate-400">Header</header>)
+}
