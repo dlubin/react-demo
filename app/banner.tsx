408acb2fe91ed1bc465a32f48dffbbb6bd5e88d0
David Lubin <lubin.davidg@gmail.com>
2023-05-09 01:04:54 -0400

    Banner placeholder

---
+++ app/banner.tsx
@@ -0,0 +1,41 @@
+"use client";
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'next/image';
+
+export default function Banner() {
+    return (
+        <div className="h-[480px] w-[876px] overflow-hidden">
+        <Carousel interval={2000}>
+            <Carousel.Item>
+            <Image
+                src="/banner1.jpg?"
+                alt="Banner"
+                width={875.214}
+                height={480}
+                className="mx-auto"
+                priority
+            /> 
+            </Carousel.Item>
+            <Carousel.Item>
+            <Image
+                src="/banner2.jpg?"
+                alt="Banner"
+                width={853.333}
+                height={480}
+                className="mx-auto"
+                priority
+            /> 
+            </Carousel.Item>
+            <Carousel.Item>
+            <Image
+                src="/banner3.jpg?"
+                alt="Banner"
+                width={853.333}
+                height={480}
+                className="mx-auto"
+                priority
+            /> 
+            </Carousel.Item>
+            </Carousel>
+    </div>);
+};
