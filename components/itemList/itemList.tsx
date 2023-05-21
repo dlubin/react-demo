43e6f33b162e1c40ddb843caa110ed96a564bf93
David Lubin <lubin.davidg@gmail.com>
2023-05-20 23:51:03 -0400

    More work done on item list

--- components/itemList/itemList.tsx
+++ components/itemList/itemList.tsx
@@ -1,4 +1,6 @@
 import items from './items';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function ItemList({
     header,
@@ -7,9 +9,24 @@ export default function ItemList({
     return (
         <div>
             <h1>{ header }</h1>
-            {items[category].map(({ title }, index) => (
-                <div>{ title }</div>
-            ))}
+            {items[category].length > 0 && (
+                <div className="flex gap-x-8">
+                    {items[category].map(({ title, path, id }, index) => (
+                        <Link href={`/games/${id}`} className="no-underline text-slate-400 hover:text-slate-500 transition-colors transition-ease-in">
+                            <div>
+                                <Image
+                                    src={path}
+                                    alt={title}
+                                    width={180}
+                                    height={240}
+                                    priority
+                                /> 
+                                { title }
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            )}
         </div> 
     );
 }
--- components/itemList/items.js
+++ components/itemList/items.js
@@ -2,13 +2,21 @@ export default {
     "new": [
         {
             "id": 10010,
-            "title": "TEST1",
-            "path": "/images/test1.jpg",
+            "title": "Star Wars Jedi Survivor",
+            "path": "/images/itemlist-1.avif",
         },
         {
             "id": 10011,
-            "title": "TEST2",
-            "path": "/images/test1.jpg",
+            "title": "Redfall",
+            "path": "/images/itemlist-2.avif",
+        },
+        {
+            "id": 10012,
+            "title": "Darkest Dungeon 2",
+            "path": "/images/itemlist-3.webp",
         },
     ],
+    "discounted": [
+        
+    ]
 };
