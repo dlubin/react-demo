3ffec51711b70dad20b67c5953186736ba998877
David Lubin <lubin.davidg@gmail.com>
2023-06-09 00:57:43 -0400

    Create cart icon

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,9 @@
+import Cart from '@/public/icons/cart.svg';
+
 export default function Header() {
-    return (<header className="fixed z-50 top-0 w-full px-2 h-14 flex items-center justify-between bg-gray-950 text-slate-400 border-b border-slate-400"></header>)
+    return (
+    <header className="fixed z-50 top-0 w-full px-12 h-14 grid grid-cols-6 items-center justify-items-end bg-gray-950 text-slate-400 border-b border-slate-400">
+        <button className="col-start-6 text-slate-400 fill-slate-400"><Cart width="32" height="32" /></button>
+    </header>
+    )
 }
