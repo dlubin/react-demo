d01663086b4802495b0a25d454560de429f6a525
David Lubin <lubin.davidg@gmail.com>
2023-06-20 00:03:48 -0400

    Add link to repo in header

--- components/header.tsx
+++ components/header.tsx
@@ -6,6 +6,7 @@ import Logo from '@/public/icons/cyclone.svg';
 import { useState } from 'react';
 import Cart from './cart';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header() {
     const [cartOpen, setCartOpen] = useState(false);
@@ -16,9 +17,18 @@ export default function Header() {
 
     return (
     <header className="fixed z-50 top-0 w-full bg-gray-950 text-slate-400 fill-slate-400 border-b border-slate-400">
-        <div className="h-14 grid grid-cols-[repeat(6, 1fr)] items-center max-w-[1640px] mx-auto">
+        <div className="h-14 grid grid-cols-[repeat(10,_1fr)] items-center max-w-[1640px] mx-auto">
             <Link href="/" className="flex flex-nowrap items-center gap-x-2 text-lg no-underline text-slate-400 hover:text-slate-400"><Logo width="32" height="32"></Logo><span>React Games</span></Link>
-            <div className="relative col-start-6 justify-self-end">
+            <a href="https://github.com/dlubin/react-demo" target="_blank" className="flex items-center gap-x-2 no-underline col-start-9">
+                <Image
+                    src="/icons/github-mark.png"
+                    alt="Github Logo"
+                    width={36}
+                    height={36}
+                    priority
+                ></Image>
+                <span className="text-slate-400 hover:text-slate-400">Source Code</span></a>
+            <div className="relative col-start-10 justify-self-end">
                 <button onClick={toggleCartOpen}><CartIcon width="32" height="32" /></button>
                 { cartOpen && (
                     <div className="absolute right-0 top-full rounded bg-gray-950 outline outline-2 outline-slate-400 w-max p-2">
