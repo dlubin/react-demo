191242c249c7de6e01f29bb11e58dfaff7fc4694
David Lubin <lubin.davidg@gmail.com>
2023-06-23 21:13:37 -0400

    Still trying to fix logo url issue

---
+++ components/siteLogo.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Logo from "@/public/icons/cyclone.svg";
+
+export default function SiteLogo() {
+  return (
+    <Link
+      href="/"
+      className="flex flex-nowrap items-center gap-x-2 text-lg no-underline text-slate-400 hover:text-slate-400"
+      prefetch={false}
+    >
+      <Logo width="32" height="32"></Logo>
+      <span>React Games</span>
+    </Link>
+  );
+}
