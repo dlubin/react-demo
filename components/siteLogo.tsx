6ecfa683a71a4fcec5ec71cbd766719f91b7a3fc
David Lubin <lubin.davidg@gmail.com>
2023-06-23 21:22:00 -0400

    Maybe the issue is the svg?

--- components/siteLogo.tsx
+++ components/siteLogo.tsx
@@ -8,8 +8,7 @@ export default function SiteLogo() {
       className="flex flex-nowrap items-center gap-x-2 text-lg no-underline text-slate-400 hover:text-slate-400"
       prefetch={false}
     >
-      <Logo width="32" height="32"></Logo>
-      <span>React Games</span>
+      React Games
     </Link>
   );
 }
