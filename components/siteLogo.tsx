299134559bca8d0f7b4163f9679625fadd02683b
David Lubin <lubin.davidg@gmail.com>
2023-06-23 21:17:17 -0400

    Link seems to have issues with SPAs

--- components/siteLogo.tsx
+++ components/siteLogo.tsx
@@ -1,15 +1,22 @@
-import Link from "next/link";
+"use client";
+
 import Logo from "@/public/icons/cyclone.svg";
+import { useRouter } from "next/navigation";
 
 export default function SiteLogo() {
+  const router = useRouter();
+
+  function returnHome() {
+    router.push("/");
+  }
+
   return (
-    <Link
-      href="/"
+    <button
+      onClick={returnHome}
       className="flex flex-nowrap items-center gap-x-2 text-lg no-underline text-slate-400 hover:text-slate-400"
-      prefetch={false}
     >
       <Logo width="32" height="32"></Logo>
       <span>React Games</span>
-    </Link>
+    </button>
   );
 }
