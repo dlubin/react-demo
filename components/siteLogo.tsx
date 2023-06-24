7310925fcca8461c78f420d6ff7c6f479d6d694b
David Lubin <lubin.davidg@gmail.com>
2023-06-23 21:24:33 -0400

    Revert "Maybe the issue is the svg?"
    This reverts commit 6ecfa683a71a4fcec5ec71cbd766719f91b7a3fc.

--- components/siteLogo.tsx
+++ components/siteLogo.tsx
@@ -8,7 +8,8 @@ export default function SiteLogo() {
       className="flex flex-nowrap items-center gap-x-2 text-lg no-underline text-slate-400 hover:text-slate-400"
       prefetch={false}
     >
-      React Games
+      <Logo width="32" height="32"></Logo>
+      <span>React Games</span>
     </Link>
   );
 }
